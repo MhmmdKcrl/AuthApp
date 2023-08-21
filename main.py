7a80c578169168a85a872e9ae1905191cfbdeba9
mahammadkk <kocerlimehemmed2003@gmail.com>
2023-08-21 15:38:09 +0400

    add redirection route to home page

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index():
     return render_template("home.html")
 
 
+@app.route("/home-redirect")   # redirection to home page
+def home_redirect():
+    return redirect(url_for("index"))
+
+
 if __name__ == "__main__":
     app.run(port = 5000, debug = True)
